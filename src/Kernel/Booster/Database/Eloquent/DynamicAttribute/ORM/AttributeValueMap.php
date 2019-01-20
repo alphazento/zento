8f654dbb56f46ae14d73e3b35b1b98a336949546
Yongcheng Chen <tony@tonercity.com.au>
2019-01-21 10:22:53 +1100

    update

--- src/Kernel/Booster/Database/Eloquent/DynamicAttribute/ORM/AttributeValueMap.php
+++ src/Kernel/Booster/Database/Eloquent/DynamicAttribute/ORM/AttributeValueMap.php
@@ -2,5 +2,5 @@
 namespace Zento\Kernel\Booster\Database\Eloquent\DynamicAttribute\ORM;
 
 class AttributeValueMap extends \Illuminate\Database\Eloquent\Model {
-    protected $fillable = ['attribute', 'value_id', 'value'];
+    protected $fillable = ['attribute_id', 'value_id', 'value'];
 }
