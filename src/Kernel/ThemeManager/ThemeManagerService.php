913ec3d143719409ad48e0e632b85b7e20ab3880
chen <yongcheng.chen@live.com>
2020-04-26 08:34:39 +1000

    fix theme package view location

--- src/Kernel/ThemeManager/ThemeManagerService.php
+++ src/Kernel/ThemeManager/ThemeManagerService.php
@@ -5,14 +5,17 @@ namespace Zento\Kernel\ThemeManager;
 use Cookie;
 use Zento\Kernel\Facades\PackageManager;
 use Zento\Kernel\Consts;
+use Illuminate\Support\Str;
 
 class ThemeManagerService {
     protected $app;
     protected $viewFactory;
     protected $whenThemeLoadCallbacks = [];
+    protected $basePath;
 
     public function __construct($app) {
         $this->app = $app;
+        $this->basePath = base_path();
     }
 
     protected function getViewFactory() {
